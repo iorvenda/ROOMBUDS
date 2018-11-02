91f65c2bc7cd0f46d693ce95cb8e73df309ed8f2
iorvenda <iorvenda@yahoo.com>
2018-11-01 20:04:37 -0400

    Update Publicprofile.java

--- Roombuds/src/roombuds/com/Publicprofile.java
+++ Roombuds/src/roombuds/com/Publicprofile.java
@@ -18,9 +18,9 @@ import javax.servlet.http.HttpServletResponse;
 public class Publicprofile extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	 String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
-	String DB_URL = "jdbc:mysql://mysql3000.mochahost.com:3306/iorvenda_clients?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-	 String USER = "iorvenda_i";
-	 String PASS = "!@#123ASdfghj"; 
+	String DB_URL = "jdbc:mysql://servername:3306/databasename?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	 String USER = "username";
+	 String PASS = "password"; 
 	 Connection conn; 
 	Statement stmt;
 	ResultSet myRs=null;
